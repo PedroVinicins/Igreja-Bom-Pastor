14efe3cac93cc52cf6a0c421c8692a6d4e861eb2
Your Name <you@example.com>
2024-08-24 11:04:42 -0300

    sábado, 24 de agosto de 2024

--- public/js/Oscript.js
+++ public/js/Oscript.js
@@ -1,1 +1,29 @@
-import 'boxicon';
+import { database } from './config.js';
+import { ref, push } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textarea = document.querySelector('.text');
+    const submitButton = document.querySelector('input[type="button"]');
+    
+    submitButton.addEventListener('click', function() {
+        const text = textarea.value.trim();
+
+        if (text) {
+            // Cria uma referência para o nó 'cadastros'
+            const newEntryRef = ref(database, 'cadastros');
+
+            // Adiciona um novo cadastro com os dados do formulário
+            push(newEntryRef, {
+                preces: text,
+            }).then(() => {
+                alert('Preces enviadas com sucesso');
+                textarea.value = '';
+            }).catch((error) => {
+                console.error('Erro:', error);
+                alert('Tente novamente.');
+            });
+        } else {
+            alert('Por favor, preencha todos os campos.');
+        }
+    });
+});
